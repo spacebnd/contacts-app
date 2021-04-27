590cef089aa8ca05c2d9a6f210457b0c02154439
Vitaly Petrovich <spacebndx@gmail.com>
2021-04-27 21:22:18 +0700

    add contacts layout

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>Contacts</div>
+  <v-container>
+    <v-navigation-drawer app v-model="drawer"> </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Contacts</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>{{ icons.mdiLogout }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main></v-main>
+  </v-container>
 </template>
 
 <script>
+import { mdiLogout } from '@mdi/js'
+
 export default {
   name: 'Contacts',
+  data: () => ({
+    drawer: null,
+    icons: {
+      mdiLogout,
+    },
+  }),
 }
 </script>
