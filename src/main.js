bda3a8072c486a459a95b1312b307b3bcdbd4e31
Vitaly Petrovich <spacebndx@gmail.com>
2021-05-02 23:29:25 +0700

    initialize firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,25 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMQ0D1LtMl_E9bd0VP_8MfY-0Wkg_564o',
+  authDomain: 'contacts-app-df8cb.firebaseapp.com',
+  databaseURL: 'https://contacts-app-df8cb-default-rtdb.firebaseio.com',
+  projectId: 'contacts-app-df8cb',
+  storageBucket: 'contacts-app-df8cb.appspot.com',
+  messagingSenderId: '626481869046',
+  appId: '1:626481869046:web:21ad4dc86c61469dac7cd7',
+  measurementId: 'G-ZR0NQD837J',
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export const db = firebase.database()
+export const auth = firebase.auth()
 
 Vue.config.productionTip = false
 
